0186632038e7163d5e4f61e258e3b7b5eaaeb76f
Matheus Passos <msena789@gmail.com>
2021-01-17 14:48:49 -0300

    Configuring MVC architecture in project and creating controllers Categories and Articles

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const connection = require('./database/database')
 const app = express();
 
+const categoriesController = require('./controllers/CategoriesController');
+const articlesController = require('./controllers/ArticlesController');
+
 // view engine
 app.set('view engine', 'ejs');
 
@@ -19,6 +22,9 @@ connection
     .catch((error) => {
         console.error(error);
     });
+
+app.use('/', categoriesController);
+app.use('/', articlesController);
 
 app.get("/", (req, res) => {
     res.render('index');
